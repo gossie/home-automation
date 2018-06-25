c456efadb4ae16ad9329a65fe979c25a501cea86
André Schreck <andre.schreck@gmail.com>
2018-06-25 20:42:23 +0200

    return correct status code

--- gateway-service/src/main/java/com/github/gossie/home/gatewayservice/room/RoomController.java
+++ gateway-service/src/main/java/com/github/gossie/home/gatewayservice/room/RoomController.java
@@ -22,6 +22,6 @@ public class RoomController {
     @PostMapping
     public ResponseEntity<?> createRoom(@RequestBody final Room room) {
         roomService.createRoom(room);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return ResponseEntity.accepted().build();
     }
 }
